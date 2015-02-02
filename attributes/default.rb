dc0236011dc085fd32813b558577d6a9765af3bf
Danijel Maricic <danijel.maricic@nordsoftware.com>
2015-02-02 13:48:09 +0200

    assign keys to a user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
-default[:keys][:databag] = "my_keys_databag"
+default[:keys][:databag] = "name-of-the-bag"
+default[:keys][:user] = "username for key"
 default[:keys][:key_name] = "my_key"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,17 @@
-dir = "/home/chef/.ssh"
+dir = "/home/#{user}/.ssh"
 
 directory dir do
-  owner "chef"
-  group "chef"
+  owner node[:keys][:user]
+  group node[:keys][:user]
   action :create
 end
 
 data_bag_item( node[:keys][:databag], node[:keys][:key_name] ).each do |k, v|
   next unless k.match(/^id_/)
-  
+
   file "#{dir}/#{k}" do
-    owner "chef"
-    group "chef"
+    owner node[:keys][:user]
+    group node[:keys][:user]
     mode k.match(/\.pub$/) ? "644" : "600"
     content v
   end
